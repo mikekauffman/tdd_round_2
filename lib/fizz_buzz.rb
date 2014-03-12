61f5d5ef9c2c4fed9833fa8951545c3ae728d815
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-11 20:01:25 -0600

    program will return an array from 1 to any number, changing multiples of 3 to 'fizz'

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -3,7 +3,24 @@ class FizzBuzz
     @highest_num = highest_num
   end
   def fizzarray
-    arrayed = 1.upto(@highest_num).to_a
-    arrayed
+    first_array = 1.upto(@highest_num).to_a
+
+    i = 0
+
+    while (i < first_array.length)
+
+      if (is_multiple_of?(3, first_array[i]))
+        first_array[i] = "fizz"
+      end
+
+      i += 1
+    end
+
+    first_array
+  end
+
+  def is_multiple_of?(divisible_by, number)
+    number % divisible_by == 0
+
   end
 end
