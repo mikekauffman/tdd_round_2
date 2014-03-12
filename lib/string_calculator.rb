294aa95ac2256a292137712c596fcbb886c46845
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-12 10:37:07 -0600

    refactored code to remove the put method which was fudging test returns

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -8,15 +8,13 @@ class StringCalc
     @string = string
     if string.empty?
       0
-    end
     else
     numbers = string.split(@delimiter)
     sum = 0
     numbers.each do |n|
-      puts n
       sum += n.to_i
     end
     sum
+    end
   end
-
 end
