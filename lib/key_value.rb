495d417ac5b3408a9e6b8eae4809633a7e9070be
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-15 18:38:46 -0600

    allows a list of keys to be displayed

--- lib/key_value.rb
+++ lib/key_value.rb
@@ -8,4 +8,14 @@ class KeyValue
   def get (key)
     @store[key]
   end
+  def delete(key)
+    @store.delete(key)
+  end
+  def list
+    list = Array.new
+    @store.each do |k,v|
+    list << k
+    end
+  list
+  end
 end
--- spec/key_value_spec.rb
+++ spec/key_value_spec.rb
@@ -9,4 +9,21 @@ describe "KeyValue" do
   actual = store.get('foo')
   expect(actual).to eq expected
   end
+  it "allows a key to be deleted" do
+  store = KeyValue.new
+  store.add('mike', 1500)
+  store.delete('mike')
+  expected = nil
+  actual = store.get('mike')
+  expect(actual).to eq expected
+  end
+  it "allows a user to see a list of keys" do
+  store = KeyValue.new
+  store.add('mike', 1000)
+  store.add('jim', 1500)
+  store.add('brit', 500)
+  expected = ["mike", "jim", "brit"]
+  actual = store.list
+  expect(actual).to eq expected
+  end
 end
