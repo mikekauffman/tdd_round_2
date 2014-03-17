e3e088881f766f8fc6836f39d2a22663837aba8d
mikekauffman <mike.a.kauffman@gmail.com>
2014-03-16 22:37:41 -0600

    allows a user to add, subtract, store a number in memory, retrieve a number from memory, and clear the memory

--- lib/numeric_calc.rb
+++ lib/numeric_calc.rb
@@ -1,9 +1,21 @@
 class Numeric
 
-  def initialize
-
+  def initialize(*memory)
+    @memory
   end
   def add(number1, number2)
     number1 + number2
+  end
+  def subtract(number1, number2)
+    number1 - number2
+  end
+  def save(number)
+    @memory = number
+  end
+  def get
+    @memory
+  end
+  def clear
+    @memory = 0
   end
 end
--- spec/numeric_calc_spec.rb
+++ spec/numeric_calc_spec.rb
@@ -8,4 +8,30 @@ describe Numeric do
     actual = numbers.add(60, 40)
     expect(actual).to eq expected
   end
+  it "takes the difference of two numbers" do
+    numbers = Numeric.new
+    expected = 20
+    actual = numbers.subtract(60, 40)
+    expect(actual).to eq expected
+  end
+  it "allows a user to save a number in the calc" do
+    numbers = Numeric.new
+    expected = 100
+    actual = numbers.save(100)
+    expect(actual).to eq expected
+  end
+  it "allows a user to retrieve a number from the calc memory" do
+    numbers = Numeric.new
+    numbers.save(100)
+    expected = 100
+    actual = numbers.get
+    expect(actual).to eq expected
+  end
+  it "allows a calc memory to be cleared" do
+    numbers = Numeric.new
+    numbers.save(100)
+    expected = 0
+    actual = numbers.clear
+    expect(actual).to eq expected
+  end
 end
